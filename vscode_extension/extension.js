1b98654c18596e2a7d618b5f222e1c653b0f06bc
komiyamma <komiyama0629@gmail.com>
2025-02-08 19:23:15 +0900

    ワークスペースを切り替えてもソースリポジトリへと移動してしまう問題の修正

--- vscode_extension/extension.js
+++ vscode_extension/extension.js
@@ -1,7 +1,30 @@
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+// 一時フォルダのパス (OSによって異なるので注意)
+const tempDir = process.env.TEMP || process.env.TMP;
+
+// ファイル名
+const fileName = 'HmOpenVSCodeFromHidemaru.txt';
+
+// ファイルのフルパス
+const fileFullPath = path.join(tempDir, fileName);
 
 function activate(context) {
-    vscode.commands.executeCommand('workbench.view.scm');
+
+    // ファイルの存在を確認し、存在すれば削除する
+    fs.access(fileFullPath, fs.constants.F_OK, (err) => {
+        if (err) {
+            ; // ファイルは存在しない
+        } else {
+            // ソースリポジトリの表示
+            vscode.commands.executeCommand('workbench.view.scm');
+
+            // ファイルが存在する場合は削除する
+            fs.unlink(fileFullPath, (err) => { });
+         }
+    });
 }
 
 function deactivate() {}
